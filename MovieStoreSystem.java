32c0cfdc5d9e2e63ccb4bfa5d96db593b6bee1d3
Sam Yusuf Azie <yusuf4c@yahoo.com>
2026-01-03 11:17:33 +0800

    blank

--- MovieStoreSystem.java
+++ MovieStoreSystem.java
@@ -1,113 +1,127 @@
 package MovieStoreSystem;
 import java.util.ArrayList;
 import java.util.Scanner;
-public class MovieStoreSystemBeforeRefactoring {
+
+public class MovieStoreSystemRefactoredP1 {
 	public static void main(String[] args) {
 		
-		//This code is to add movie titles to the movie store and sort them
-		ArrayList<String> MST = new ArrayList<String>(); // an array list of movie titles
-		String nmt; //A variable to add a new movie title
-		String elementToSearch = null;
-		Boolean Found;
+		// This code is to add movie titles to the movie store and sort them
+		ArrayList<String> movieTitles = new ArrayList<String>(); // an array list of movie titles
+		String newMovieTitle; //A variable to add a new movie title
+		
+		
+
 		System.out.println("Create the movies list by entering the movie titles one by one");		
 		System.out.println("Enter a movie title to be added to the movie store");
 		Scanner input = new Scanner(System.in);		
-		nmt = input.next();		
-		while (!(nmt.equals("end"))) {		
-			MST.add(nmt);
+		newMovieTitle = input.next();		
+		while (!(newMovieTitle.equals("end"))) {		
+			movieTitles.add(newMovieTitle);
 			System.out.print("Enter a movie title to be added to the movie store");
-			nmt = input.next();			
+			newMovieTitle = input.next();			
 			}
 		System.out.println("The movie titles in the movie store before sorting are: ");
-		   for(String counter: MST){
-				System.out.println(counter);
+		   for(String title: movieTitles){
+				System.out.println(title);
 			}
-		 //bubble sort--------------------------------------------------
-		   boolean sorted = false;
-		    String temp = null;
-		    while(!sorted) {
-		        sorted = true;
-		        for (int i = 0; i < MST.size() - 1; i++) {		           
-		        	if((MST.get(i)).compareToIgnoreCase(MST.get(i+1))>0) {
-		                temp = MST.get(i);
-		                MST.set(i,MST.get(i+1));
-		                MST.set(i+1,temp);
-		                sorted = false;
+
+
+		 //bubble sor movies--------------------------------------------------
+		   boolean isMoviesSorted = false;
+		    String movieTemp = null;
+		    while(!isMoviesSorted) {
+		        isMoviesSorted = true;
+		        for (int i = 0; i < movieTitles.size() - 1; i++) {		           
+		        	if((movieTitles.get(i)).compareToIgnoreCase(movieTitles.get(i+1))>0) {
+		                movieTemp = movieTitles.get(i);
+		                movieTitles.set(i,movieTitles.get(i+1));
+		                movieTitles.set(i+1,movieTemp);
+		                isMoviesSorted = false;
 		            }
 		        }
 		    }
 		 //-------------------------------------------------------
 		 System.out.println("The movie titles in the movie store after sorting are: ");
-		   for(String counter: MST){
-				System.out.println(counter);
+		   for(String title: movieTitles){
+				System.out.println(title);
 			}  	   
+
+
 		 //This code is to add members to the movie store and sort them
-			ArrayList<String> MSM = new ArrayList<String>();// an array list of movie store members
-			String nMn;//A variable to add a new member name
+			ArrayList<String> memberNames = new ArrayList<String>();// an array list of movie store members
+			String newMemberName;//A variable to add a new member name
+
 			System.out.println("Create the members list by entering the member names one by one");		
 			System.out.println("Enter a member name to be added to the movie store system");
-			input = new Scanner(System.in);		
-			nMn = input.next();		
-			while (!(nMn.equals("end"))) {		
-				MSM.add(nMn);
+			
+			newMemberName = input.next();		
+			while (!(newMemberName.equals("end"))) {		
+				memberNames.add(newMemberName);
 				System.out.print("Enter a member name to be added to the movie store");
-				nMn = input.next();			
+				newMemberName = input.next();			
 				}
+
 			System.out.println("The members in the movie store before sorting are: ");
-			   for(String counter: MSM){
-					System.out.println(counter);
+			   for(String name: memberNames){
+					System.out.println(name);
 				}
-			 //bubble sort--------------------------------------------------
-			   sorted = false;
-			   temp = null;
-			    while(!sorted) {
-			        sorted = true;
-			        for (int i = 0; i < MSM.size() - 1; i++) {		           
-			        	if((MSM.get(i)).compareToIgnoreCase(MSM.get(i+1))>0) {
-			                temp = MSM.get(i);
-			                MSM.set(i,MSM.get(i+1));
-			                MSM.set(i+1,temp);
-			                sorted = false;
+
+
+			 //bubble sort member--------------------------------------------------
+			   boolean isMemberSorted = false;
+			   String memberTemp = null;
+			    while(!isMemberSorted) {
+			        isMemberSorted = true;
+			        for (int i = 0; i < memberNames.size() - 1; i++) {		           
+			        	if((memberNames.get(i)).compareToIgnoreCase(memberNames.get(i+1))>0) {
+			                memberTemp = memberNames.get(i);
+			                memberNames.set(i,memberNames.get(i+1));
+			                memberNames.set(i+1,memberTemp);
+			                isMemberSorted = false;
 			            }
 			        }
 			    }
+
+
 			 //-------------------------------------------------------
 			 System.out.println("The members in the movie store after sorting are: ");
-			   for(String counter: MSM){
-					System.out.println(counter);
+			   for(String name: memberNames){
+					System.out.println(name);
 				}		   
 			  			   
+
+
 			 //This code is to search for a specific movie title
 		     System.out.println("Enter a movie title to search for:");
-			   elementToSearch = input.next();	
-			   Found = false;	
+			   String movieToSearch = input.next();	
+			   boolean isMovieFound = false;	
 			   
 		   //Linear Search----------------------------------------------
-		   for (int index = 0; index < MST.size(); index++) {
-		        if (MST.get(index).equals(elementToSearch)) 
-		        	Found = true;     
+		   for (int index = 0; index < movieTitles.size(); index++) {
+		        if (movieTitles.get(index).equals(movieToSearch)) 
+		        	isMovieFound = true;     
 		    }
 		   //------------------------------------------------------------
-		   if (Found == true)
+		   if (isMovieFound == true)
 		       System.out.println("The movie title is found");
-		   else if (Found == false)
-				   System.out.println("The movie title is not found");
+		   else 
+				System.out.println("The movie title is not found");
 		   //--------------------------------------------------------------
 		   
 		 //This code is to search for a specific member
 		     System.out.println("Enter a member name to search for:");
-			   elementToSearch = input.next();	
-			   Found = false;	
+			   String memberToSearch = input.next();	
+			   boolean isMemberFound = false;	
 			   
 		   //Linear Search----------------------------------------------
-		   for (int index = 0; index < MSM.size(); index++) {
-		        if (MSM.get(index).equals(elementToSearch)) 
-		        	Found = true;     
+		   for (int index = 0; index < memberNames.size(); index++) {
+		        if (memberNames.get(index).equals(memberToSearch)) 
+		        	isMemberFound = true;     
 		    }
 		   //------------------------------------------------------------
-		   if (Found == true)
+		   if (isMemberFound == true)
 		       System.out.println("The member is found");
-		   else if (Found == false)
+		   else if (isMemberFound == false)
 				   System.out.println("The member is not found");
 		   //--------------------------------------------------------------
 	}
